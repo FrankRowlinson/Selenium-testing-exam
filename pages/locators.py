267d2086d548b6272acbe837d7bfc7170c0c9ec9
Vladislav Smolov <johk2293@gmail.com>
2022-07-14 14:31:16 +0400

    add product page tests

--- pages/locators.py
+++ pages/locators.py
@@ -12,3 +12,12 @@ class LoginPageLocators:
     REGISTER_USERNAME = (By.ID, "#id_registration-email")
     REGISTER_PASSWORD = (By.ID, "#id_registration-password1")
     REGISTER_PASSWORD_REPEAT = (By.ID, "#id_registration-password2")
+
+
+class ProductPageLocators:
+    ADD_TO_BASKET_BUTTON = (By.CLASS_NAME, "btn-add-to-basket")
+    PRODUCT_PRICE = (By.CSS_SELECTOR, "div.product_main p.price_color")
+    # should check with .endswith(), cause text contains jibberish
+    PRODUCT_TITLE = (By.CSS_SELECTOR, "div.product_main h1")
+    ADDED_PRODUCT_TITLE = (By.CSS_SELECTOR, "#messages div:nth-child(1) strong")
+    BASKET_TOTAL = (By.CSS_SELECTOR, "#messages div:nth-child(3) strong")
---
+++ test_product_page.py
@@ -0,0 +1,11 @@
+from pages.product_page import ProductPage
+import time
+
+
+def test_guest_can_add_product_to_basket(browser):
+    link = "http://selenium1py.pythonanywhere.com/catalogue/the-shellcoders-handbook_209/?promo=newYear"
+    page = ProductPage(browser, link)
+    page.open()
+    page.add_product_to_basket()
+    page.should_be_equal_product_price_and_basket_total()
+    page.should_be_correct_added_product()
