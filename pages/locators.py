35c2bcda64bb1dcb05797293a01d740bb5a50618
Vladislav Smolov <johk2293@gmail.com>
2022-07-14 12:44:32 +0400

    add locators, login page tests, refactor

---
+++ pages/locators.py
@@ -0,0 +1,14 @@
+from selenium.webdriver.common.by import By
+
+
+class MainPageLocators:
+    LOGIN_LINK = (By.CSS_SELECTOR, "#login_link")
+
+
+class LoginPageLocators:
+    LOGIN_USERNAME = (By.ID, "#id_login-username")
+    LOGIN_PASSWORD = (By.ID, "#id_login-password")
+
+    REGISTER_USERNAME = (By.ID, "#id_registration-email")
+    REGISTER_PASSWORD = (By.ID, "#id_registration-password1")
+    REGISTER_PASSWORD_REPEAT = (By.ID, "#id_registration-password2")
